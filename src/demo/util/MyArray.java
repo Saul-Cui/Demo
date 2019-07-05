71ecdd7e8b4abe64f878637d6d4235b68f19db3d
Saul_Cui <cdmkly@qq.com>
2019-07-05 16:20:19 +0800

    array object

--- src/demo/util/MyArray.java
+++ src/demo/util/MyArray.java
@@ -2,4 +2,16 @@ package demo.util;
 
 public class MyArray {
 
+    private int[] elements;
+
+    public MyArray()
+    {
+        elements = new int[0];
+    }
+
+    public int size()
+    {
+        return elements.length;
+    }
+
 }
