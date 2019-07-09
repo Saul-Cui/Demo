6cafaa200c0252922c9613e7f2ce255503b0ed58
Saul Cui <cdmkly@qq.com>
2019-07-09 20:54:04 +0800

    BinarySearch

---
+++ src/demo1/TestBinarySearch.java
@@ -0,0 +1,35 @@
+package demo1;
+
+public class TestBinarySearch {
+
+    public static void main(String[] args)
+    {
+        int[] arr = new int[] {1,2,3,4,5,6,7,8,9};
+        int target = 8;
+
+        int begin = 0;
+        int end = arr.length - 1;
+        int mid = (begin + end) / 2;
+
+        int index = -1;
+        while (true)
+        {
+            if (arr[mid] == target)
+            {
+                index = mid;
+                break;
+            } else {
+                if (arr[mid] > target)
+                {
+                    end = mid - 1;
+                } else {
+                    begin = mid + 1;
+                }
+                mid = (begin + end) / 2;
+            }
+        }
+
+        System.out.println(index);
+        //return index;
+    }
+}
